1fb702b8efb799e3926a679761c3e69f09ac63df
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-08 21:57:02 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//        Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan
+//        3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("3 ve 4 'e tam bölünenleri bulmak istediniz sayıyı giriniz: ");
+        int sayi= scanner.nextInt();
+        int toplam = 0; // Bölünen sayıların toplamını tutacak değişken
+        int adet = 0;   // Bölünen sayı adedini tutacak değişken
+
+        for (int i = 1; i < sayi; i++) {
+            if(i%12==0){
+                System.out.println(i+" ");
+                toplam+=i;
+                adet++;
+            }
+        }
+        if (adet>0){
+            double ortalama=(double) toplam/adet;
+            System.out.println("Ortalama: " + ortalama);
+        }else  {
+            System.out.println("Girilen aralıkta 3 ve 4 e tam bölenen sayı yoktur.");
         }
     }
 }
+
